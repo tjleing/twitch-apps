bc9abca7d43c8bfc78de81e77d4f2a8847b59cf8
bluezebragames <bluezebragames@gmail.com>
2022-07-30 19:15:31 -0700

    update file list

--- intro-photo-switcher/filenames.js
+++ intro-photo-switcher/filenames.js
@@ -1,3 +1,3 @@
 let filenames = 
-['respect_what_is_it.jpeg', 'shucka_trucka.png', 'bears.png', 'father.png', 'cold_case.png']
+['respect_what_is_it.jpeg', 'shucka_trucka.png', 'oven.png', 'bears.png', 'father.png', 'cold_case.png']
 export {filenames};
