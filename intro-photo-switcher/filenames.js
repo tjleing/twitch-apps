6d08934690bcc826587bf0e2cedcc8f96bc8a0d7
bluezebragames <bluezebragames@gmail.com>
2022-08-17 17:04:14 -0700

    add photo

--- intro-photo-switcher/filenames.js
+++ intro-photo-switcher/filenames.js
@@ -1,3 +1,3 @@
 let filenames = 
-['respect_what_is_it.jpeg', 'chateau.png', 'shucka_trucka.png', 'shorts.png', 'oven.png', 'bears.png', 'father.png', 'cold_case.png']
+['respect_what_is_it.jpeg', 'chateau.png', 'shucka_trucka.png', 'apnea.png', 'shorts.png', 'oven.png', 'bears.png', 'father.png', 'cold_case.png']
 export {filenames};
