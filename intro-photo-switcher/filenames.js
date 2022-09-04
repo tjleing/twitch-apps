cc885caeb224ec75929c234f2811e16995c6b0be
bluezebragames <bluezebragames@gmail.com>
2022-09-03 23:14:28 -0700

    add images

--- intro-photo-switcher/filenames.js
+++ intro-photo-switcher/filenames.js
@@ -1,3 +1,3 @@
 let filenames = 
-['gaslighting.png', 'bigfoot2.jpeg', 'passion.png', 'respect_what_is_it.jpeg', 'chateau.png', 'shucka_trucka.png', 'apnea.png', 'lactase.png', 'shorts.png', 'blocked.png', 'stairs.png', 'oven.png', 'bears.png', 'father.png', 'bigfoot.jpeg', 'cold_case.png']
+['wanderlust.png', 'A4.png', 'gaslighting.png', 'bigfoot2.jpeg', 'passion.png', 'respect_what_is_it.jpeg', 'chateau.png', 'shucka_trucka.png', 'apnea.png', 'lactase.png', 'shorts.png', 'blocked.png', 'stairs.png', 'oven.png', 'bears.png', 'father.png', 'joking.jpg', 'bigfoot.jpeg', 'cold_case.png']
 export {filenames};
