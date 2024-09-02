6633f6b35449e9ed0ecf3db367e4dd5a58a14dfd
bluezebragames <bluezebragames@gmail.com>
2024-09-02 16:35:59 -0400

    add image

--- intro-photo-switcher/import.py
+++ intro-photo-switcher/import.py
@@ -3,5 +3,5 @@ from os import listdir
 only_valid_names = lambda name: name[0] != '.'
 
 print("let filenames = ")
-print(filter(only_valid_names, listdir("img/")))
+print(list(filter(only_valid_names, listdir("img/"))))
 print("export {filenames};")
